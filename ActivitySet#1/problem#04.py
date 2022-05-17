8669ffd9cfb7ed7b093290466ec6b7febbcc627d
Supriya <>
2022-05-17 18:00:00 +0000

    completed

--- ActivitySet#1/problem#04.py
+++ ActivitySet#1/problem#04.py
@@ -1,3 +1,9 @@
 # Conditional Execution
+hrs = float(input())
+r=float(input())
+if hrs<40:
+    print("",hrs*r)
+else:
+  #  print("",hrs*r+((hrs-40)*r*0.5))
+    print(*"",hrs*r+((hrs-40)*r*0.5))
 
-hrs = input("Enter hours? ")
