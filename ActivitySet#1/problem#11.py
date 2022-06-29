f077bafe1c79b12df3eb9fd6ba98eebfd2c74b56
Supriya <97220716+Supriya2520@users.noreply.github.com>
2022-06-29 17:43:17 +0000

    completed

--- ActivitySet#1/problem#11.py
+++ ActivitySet#1/problem#11.py
@@ -1,3 +1,18 @@
 # Tuples
+filename = "dataset/mbox-short.txt"
+name = input("Enter file:")
 
-def input_names();
+if len(name) < 1:
+    name = "mbox-short.txt"
+counts = {}
+handle = open(name)
+for line in handle:
+    if not line.startswith('From '):
+        continue
+    x = line.replace(':',' ')
+    words = x.split()
+    y = words[5:6]
+    for word in y:
+        counts[word] = counts.get(word,0) + 1
+lst = []
+
