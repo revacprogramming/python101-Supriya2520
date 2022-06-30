2f0d036afef05db119d0318b0ee8c2687b9bf68c
Supriya <97220716+Supriya2520@users.noreply.github.com>
2022-06-30 17:41:36 +0000

    completed

--- ActivitySet#1/problem#10.py
+++ ActivitySet#1/problem#10.py
@@ -1,6 +1,17 @@
 # Dictionaries
-n=4
-whlie n>0
-print(n)
-n=n-1
+files  = open("mbox-short.txt")
+for line in files:
+    if line.startswith("From"):
+        words = line.split()
 
+        if len(words) > 6:
+            counts[words[1]] = counts.get(words[1], 0) + 1
+bigcount = None
+bigword = None
+
+for word, count in counts.items():
+    if bigcount is None or count > bigcount:
+        bigcount = count
+        bigword = word
+        
+print(bigword, bigcount)
