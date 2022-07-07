c175f50ad85ca4908d70bd06842fbd345a19c2fd
Supriya <97220716+Supriya2520@users.noreply.github.com>
2022-07-07 17:52:38 +0000

    completed

--- ActivitySet#1/problem#18.py
+++ ActivitySet#1/problem#18.py
@@ -1,2 +1,25 @@
 #using web services
 #Extracting Data from XML
+import urllib.request, urllib.parse, urllib.error
+import xml.etree.ElementTree as ET
+import ssl
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+while True:
+    url = input('Enter location: ')
+    if len(url) < 1: break
+    xml = urllib.request.urlopen(url, context=ctx).read()
+    print('Retrieving', url)
+    print('Retrieved', len(xml), 'characters')
+    commentinfo = ET.fromstring(xml)
+    lst = commentinfo.findall('comments/comment')
+    sum = 0
+    count = 0
+    for item in lst:
+        count = count + 1
+        sum = sum + int(item.find('count').text)
+    print('Count:', count)
+    print('Sum:', sum)
+
--- ActivitySet#1/problem#19.py
+++ ActivitySet#1/problem#19.py
@@ -1,1 +1,17 @@
 # Extract Data from JSON
+import json, urllib.request
+
+
+url = input('Enter URL:')
+if len(url) < 1: url = 'http://py4e-data.dr-chuck.net/comments_1465666.json'
+data = urllib.request.urlopen(url).read().decode()             # URL Request/Open()/Read()/Decode()
+total = []
+info = json.loads(data)
+
+#  print(json.dumps(info, indent=4))                           # print JSON in readable form for Analysis
+print(len(data), 'Characters found')
+for i in info['comments']:                                     # iterate for every element in 'comments'
+    total.append(i['count'])                                   # append contnents of 'count' to 'total' - (list)
+
+print('Sum of "comments":', sum(total))
+ 
