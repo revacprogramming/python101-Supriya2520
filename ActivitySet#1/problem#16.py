e2e5345cdd1f79c1403d378abc73a94fdffb7432
Supriya <97220716+Supriya2520@users.noreply.github.com>
2022-07-07 19:16:12 +0000

    completed

--- ActivitySet#1/problem#16.py
+++ ActivitySet#1/problem#16.py
@@ -1,4 +1,54 @@
+#import modules
 
+import urllib.request
+
+import urllib.error
+
+import urllib.parse
+
+from bs4 import BeautifulSoup
+
+import ssl
+
+ 
+
+# Https protocol, for those you didn't understood this is when we put https instead of http
+
+ctx = ssl.create_default_context()
+
+ctx.check_hostname = False
+
+ctx.verify_mode = ssl.CERT_NONE
+
+ 
+
+url = input('Enter - ')
+
+html = urllib.request.urlopen(url, context=ctx).read()
+
+soup = BeautifulSoup(html, 'html.parser')
+
+ 
+
+#we create a list here as we'll store all values in the list and then print the sum of list
+
+lst = list()
+
+#have to target span tag as given in assignment
+
+tags = soup('span')
+
+for tag in tags:
+
+    Contents = tag.contents[0]
+
+    c = int(Contents)
+
+    lst.append(c)
+
+print(sum(lst))
+
+ 
 
 
 
--- ActivitySet#1/problem#17.py
+++ ActivitySet#1/problem#17.py
@@ -1,4 +1,26 @@
-
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+import ssl
+# Ignore certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+# Open the original URL
+url = input('Enter URL: ')
+html = urllib.request.urlopen(url, context=ctx).read()
+soup = BeautifulSoup(html, 'html.parser')
+# Read the count and position from the user
+count = input('Enter count: ')
+position = input('Enter position: ')
+print('Retrieving:', url)
+# The iteration
+for i in range(int(count)):
+    tags = soup('a')
+    urlpos = tags[int(position) - 1]
+    url = urlpos.get('href', None)
+    print('Retrieving:', url) # and then open the target URL in each page
+    html = urllib.request.urlopen(url, context=ctx).read()
+    soup = BeautifulSoup(html, 'html.parser')
 
 
 
